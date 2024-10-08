9869f48f29a0d45d7a19891ed832c05be1e7e5fe
Pranav Malhotra <pranav.malhotra1903@example.com>
2024-10-09 02:20:06 +0530

    new elements

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import {Box, Stack , Typography, Button} from '@mui/material';
+import {Box, Typography, Button} from '@mui/material';
 import HeroBannerImage from '../assets/assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
     <Box sx={{
-        mt: { lg:'212px', xs:'70px'},
-        ml :{sm:'50px'}
-    }}position='relative' p='20px'>
-        <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+        mt: { lg:'250px', xs:'70px'},
+        ml :{sm:'100px'}
+    }}position='relative' p='20px' left='100px'>
+        <Typography color='#FF2625' fontWeight='600' fontSize='34px' >
             Fitness Club
         </Typography>
-        <Typography fontFamily="Arial" fontWeight={700} sx={{fontSize:{lg:'44px',xs:'40px'}}}>
+        <Typography fontFamily="Arial" fontWeight={700} sx={{fontSize:{lg:'42px',xs:'38 px'}}}
+        mb='15px' mt='20px'>
         Work Hard, Smile often, Keep Going!
         </Typography>
         <Typography fontSize='26px' lineHeight='35px' mb={3}>
             Checkout the most effective Exercise
         </Typography>
-        <Button variant='contained' color='error' href='#exercise'>
-            Explore Exercises
+        <Button variant='contained' color='error' href='#exercise' sx={{backgroundColor:'#ff2625',padding:'10px'}}>
+            Explore Exercises   
         </Button>
+        <Typography
+        fontWeight={600}
+        color='#ff2625'
+        fontSize="200px"
+        sx={{
+            opacity:0.1,
+            display:{lg:'block', xs:'none'}
+        }}
+        >Exercise</Typography>
         <img src={HeroBannerImage} alt="banner" className='hero-banner-img' />
 
     </Box>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Stack } from '@mui/material';
-import Logo from '../assets/assets/images/Logo.png'
-import { BorderBottom, Height } from '@mui/icons-material';
+import { Stack, Typography } from '@mui/material';
+import Logo from '../assets/assets/images/Logo.png';
+
 
 const Navbar = () => {
   return (
     <Stack
       direction='row'
-      justifyContent='space-around' sx={{gap :{sm:'122px',xs:'40px'},mt:{sm:'32px',xs:'20px'},justifyContent:'none'}} px="20px">
-      <Link to='/' >
-        <img src={Logo} alt="logo" style={{
-          width: '48px', Height: '48px', margin: '0 20px'
-        }} />
-      </Link>
+      justifyContent='space-between'
+      sx={{
+        gap: { sm: '122px', xs: '40px' },
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none'
+      }}
+      px="20px"
+    >
+      <Stack direction="row" alignItems="center" gap="10px">
+        <Link to='/'>
+          <img
+            src={Logo}
+            alt="logo"
+            style={{
+              width: '50px',
+              height: '50px', // "Height" should be lowercase
+            }}
+          />
+        </Link>
+        <Link to='/' style={{ textDecoration: "none" }}>
+          <Typography fontWeight={700} sx={{ textDecoration: "none", color: "#3A1212", fontSize:{lg:'50px',xs:'46px'},fontFamily:'Bebas Neue'}}>
+            TrainRight
+          </Typography>
+        </Link>
+      </Stack>
+
       <Stack
         direction='row'
         gap='40px'
-        fontSize='24px'
+        fontSize='30px'
         alignItems='flex-end'
       >
         <Link to='/' style={{
-        textDecoration:"none", color:"#3A1212", BorderBottom:"3px solid #FF2625"
-      }}>Home</Link>
+          textDecoration: "none", color: "#3A1212", borderBottom: "3px solid #FF2625"
+        }}>Home</Link>
         <a href="#excercises" style={{
-          textDecoration: 'none', color:'#3A1212'
+          textDecoration: 'none', color: '#3A1212'
         }}>Exercises</a>
       </Stack>
     </Stack>
-
-  )
+  );
 }
 
 export default Navbar;
