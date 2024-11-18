2681495dac7a260a461bcc5d0c47062673aafb5b
Pranav Malhotra <pranav.malhotra1903@gmail.com>
2024-11-18 10:38:35 +0530

    search exercise

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,27 +1,64 @@
-import React ,{useState, useEffect} from 'react';
-import {Box, Button, Stack ,TextField, Typography} from '@mui/material'; 
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
-      <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'30px',mb:'50px' ,textAlign:'center'}}}>
-      Great Workouts You <br />
-      Should Try
-      </Typography>
-      <Box position='relative' mb='72px'>
-        <TextField 
+    <Stack
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: '100vh',
+        width: '100vw', // Full viewport width
+        display: 'flex',
+        textAlign: 'center',
+        backgroundColor: '#f5f5f5', // Optional background color
+      }}
+    >
+      <Typography
+        fontWeight={700}
         sx={{
-          input:{fontWeight:'700',border:'none', borderRadius:'4px'}
+          fontSize: { lg: '44px', xs: '30px' },
+          mb: '50px',
+          textAlign: 'center',
+          maxWidth: '600px',
         }}
-        height='76px' value='' 
-        onChange={(e)=>{}}
-        placeholder='Search Exercises'
-        type='text' />
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+            marginLeft: '10px',
+          }}
+        >
+          Search
+        </Button>
       </Box>
-
     </Stack>
+  );
+};
 
-  )
-}
-
-export default SearchExercises
+export default SearchExercises;
